1e32c804b9d070ad332184ca7184fce57a89131e
LiamTran <crackcodepow@gmail.com>
2021-09-09 21:05:33 +0700

    first commit

---
+++ freecodecamp.py
@@ -0,0 +1,31 @@
+# #Expressions
+# #Numberic expressions (/,**,%)
+# #Types(int, string, boolean)
+# #Python CANNOT connect int + string
+
+# #type():
+
+# #Type conversion:
+# print(float(99)+100)
+
+# i = 42
+# f = float(i)
+# print
+# print(f)
+# sval = '123'
+# ival = int(sval)
+# print(ival+1)
+
+# #user input
+# name = input("Tell me ur name:")
+# print(f"Welcome, {name}")
+
+# #converting input, input is a string -> must convert it to int
+# #Case using a conversion
+# floorNumEU = int(input("What floor do you want"))
+# floorNumUS = floorNumEU + 1
+# print(f'US floor:{floorNumUS}')
+
+#Condition Execution (if statements)
+#two-way (if - else) either 1 or 2
+
